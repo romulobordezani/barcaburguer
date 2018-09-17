0e9fdb7f9c73346d79c8e14af8edd66ebca97f9c
travis_barca <romulobordezani@gmail.com>
2018-09-16 22:36:53 -0300

    PWA tryout

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
-                '/barcaburguer/',
-                '/barcaburguer/index.html'
+                '/',
+                '/index.html'
             ]);
         })
     );
