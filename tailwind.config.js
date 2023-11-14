3d39c4c23ada6048850cb00dd5aa89c4b8578be2
Romulo Bordezani <romulo.bordezani@pluto.tv>
2023-11-14 16:10:21 -0300

    TailwindVersion

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'daily-dev-tips': '#F89283',
+        'barca-black': '#303030',
+      },
+    },
+  },
+  plugins: [],
+}
+
